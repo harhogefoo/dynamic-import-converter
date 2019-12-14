ffb478333bac146b6b46bb6d35b486df25f34802
harhogefoo <harimanmon@gmail.com>
2019-12-14 21:20:18 +0900

    chore: update demo

--- demo/index.vue
+++ demo/index.vue
@@ -8,11 +8,13 @@
 <script>
 import Hoge from "@/components/Hoge.vue"
 import Piyo from "@/components/Piyo.vue"
+import Bar from '@/components/Piyo.vue'
 
 export default {
   components: {
     Hoge,
-    Piyo
+    Piyo,
+    Bar
   }
 }
 </script>
